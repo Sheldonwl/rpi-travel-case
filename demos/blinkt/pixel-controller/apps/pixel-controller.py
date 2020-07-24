fe744a63313a7bf7d1876ae23e047058284fe074
Sheldon Lo-A-Njoe <6817520+Sheldonwl@users.noreply.github.com>
2020-07-24 13:02:38 +0100

    Imporove pixel-controller

--- demos/blinkt/pixel-controller/apps/pixel-controller.py
+++ demos/blinkt/pixel-controller/apps/pixel-controller.py
@@ -4,83 +4,25 @@ import time
 import blinkt
 import os
 
-no_pixels = True
 dir = "/home/pi/pixels/"
-BRIGHTNESS = os.environ["BRIGHTNESS"]
-CLEAR_SLEEP = os.environ["CLEAR_SLEEP"]
+BRIGHTNESS = 255
+CLEAR_SLEEP = 0.1
 
 while True:
-    if os.path.isfile(dir + str(0)):        # Pixel 0
-        blinkt.set_pixel(0, 0, 0, int(BRIGHTNESS))
-        no_pixels = False
-    else: 
-        blinkt.set_pixel(0, 0, 0, 0)
-        no_pixels = True
-    if os.path.isfile(dir + str(1)):        # Pixel 1
-        blinkt.set_pixel(1, 0, 0, int(BRIGHTNESS))
-        no_pixels = False
-    else: 
-        blinkt.set_pixel(1, 0, 0, 0)
-        no_pixels = True
-    if os.path.isfile(dir + str(2)):        # Pixel 2
-        blinkt.set_pixel(2, 0, 0, int(BRIGHTNESS))
-        no_pixels = False
-    else: 
-        blinkt.set_pixel(2, 0, 0, 0)
-        no_pixels = True
-    if os.path.isfile(dir + str(3)):        # Pixel 3
-        blinkt.set_pixel(3, 0, 0, int(BRIGHTNESS))
-        no_pixels = False
-    else: 
-        blinkt.set_pixel(3, 0, 0, 0)
-        no_pixels = True
-    if os.path.isfile(dir + str(4)):        # Pixel 4
-        blinkt.set_pixel(4, 0, 0, int(BRIGHTNESS))
-        no_pixels = False
-    else: 
-        blinkt.set_pixel(4, 0, 0, 0)
-        no_pixels = True
-    if os.path.isfile(dir + str(5)):        # Pixel 5
-        blinkt.set_pixel(5, 0, 0, int(BRIGHTNESS))
-        no_pixels = False
-    else: 
-        blinkt.set_pixel(5, 0, 0, 0)
-        no_pixels = True
-    if os.path.isfile(dir + str(6)):        # Pixel 6
-        blinkt.set_pixel(6, 0, 0, int(BRIGHTNESS))
-        no_pixels = False
-    else: 
-        blinkt.set_pixel(6, 0, 0, 0)
-        no_pixels = True
-    if os.path.isfile(dir + str(7)):        # Pixel 7
-        blinkt.set_pixel(7, 0, 0, int(BRIGHTNESS))
-        no_pixels = False
-    else: 
-        blinkt.set_pixel(7, 0, 0, 0)
-        no_pixels = True
-    
+    no_pixels = 8
+    for i in range(0,8):
+        if os.path.isfile(dir + str(i)):
+            blinkt.set_pixel(i, 0, 0, int(BRIGHTNESS))
+            no_pixels += 1
+        else:
+            blinkt.set_pixel(i, 0, 0, 0)
+            no_pixels -= 1
+
     blinkt.show()
-    time.sleep(float(CLEAR_SLEEP))
+    time.sleep(int(CLEAR_SLEEP))
 
-    if no_pixels == True:
-        blinkt.clear()
-        blinkt.set_pixel(0, 0, 0, 0)
-        blinkt.set_pixel(1, 0, 0, 0)
-        blinkt.set_pixel(2, 0, 0, 0)
-        blinkt.set_pixel(3, 0, 0, 0)
-        blinkt.set_pixel(4, 0, 0, 0)
-        blinkt.set_pixel(5, 0, 0, 0)
-        blinkt.set_pixel(6, 0, 0, 0)
-        blinkt.set_pixel(7, 0, 0, 0)
+    if no_pixels == 0:
+        for i in range(0,8):
+            blinkt.clear()
+            blinkt.set_pixel(i, 0, 0, 0)
         blinkt.show()
-
-    #blinkt.clear()
-    #blinkt.set_pixel(0, 0, 0, 0)
-    #blinkt.set_pixel(1, 0, 0, 0)
-    #blinkt.set_pixel(2, 0, 0, 0)
-    #blinkt.set_pixel(3, 0, 0, 0)
-    #blinkt.set_pixel(4, 0, 0, 0)
-    #blinkt.set_pixel(5, 0, 0, 0)
-    #blinkt.set_pixel(6, 0, 0, 0)
-    #blinkt.set_pixel(7, 0, 0, 0)
-    #blinkt.show()
